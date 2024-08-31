0cc37261fbc004c94569d1a9ab2dd54e30488faf
Yeshitha777 <yeshitha777@gmail.com>
2024-08-31 19:21:55 +0530

    Add files via upload

---
+++ log.js
@@ -0,0 +1,35 @@
+var count = 2;
+
+function validate ()
+{
+   var user = document.login.username.value;
+   var password = document.login.password.value;
+   var valid = false;
+   var usernameArray = ["yesh"]
+   var passwordArray = ["777"]
+   for (var i = 0; i < usernameArray.length; i++)
+
+if (user == usernameArray[i])
+if (password == passwordArray [i])
+{
+   valid = true;
+   break;
+}
+
+if(valid)
+{
+   alert("Login was successful");
+   window.location="log.html";
+   return false ;
+}
+var again = "tries";
+if (count ==1)
+{
+   again = "try"
+}
+if(count >= 1)
+{
+   alert("Wrong password or username")
+   count--;
+}
+}
